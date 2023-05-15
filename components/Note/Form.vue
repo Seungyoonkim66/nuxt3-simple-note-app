6ed80adaf49e829132f506325780588345fc811b
Seungyoon66 <tmddbs1998@naver.com>
2023-05-15 20:41:55 +0900

    feat: NoteForm 컴포넌트 추가 후 신규 등록 페이지에 적용

---
+++ components/Note/Form.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  title?:  string;
+  content?: string;
+}>(), {
+  title: '',
+  content: '',
+});
+
+const form = reactive({
+  title: '',
+  content: '',
+});
+
+const emits = defineEmits<{
+  (e: 'submit', value: { title: string, content: string }):void;
+}>();
+
+watch(() => props.title, title => {
+  form.title = title;
+}, { immediate: true });
+
+watch(() => props.content, content => {
+  form.content = content;
+}, { immediate: true });
+
+function submit() {
+  emits('submit', form);
+}
+</script>
+
+<template>
+  <form
+    class="form"
+    @submit.prevent="submit"
+  >
+
+    <NoteTitleField
+      v-model="form.title"
+    />
+
+    <textarea 
+      v-model="form.content"
+      class="form__content pretty-scrollbar"
+    />
+
+    <div class="form__submit-wrap`per">
+      <NoteButton 
+        class="form__submit"
+        type="submit"
+      >등록</NoteButton>
+    </div>
+  </form>
+</template>
+
+<style lang="scss" scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  &__title {
+    border: none;
+    outline: none;
+    font-size: 2.5rem;
+    margin-bottom: 1.25rem;
+    font-weight: bold;
+  }
+
+  &__content {
+    border: none;
+    outline: none;
+    font-size: 1rem;
+    flex-grow: 1;
+    resize: none;
+    margin-bottom: 1rem;
+    margin-right: -1.25rem;
+    padding-right: 1.25rem;
+    line-height: 1.4rem;
+  }
+
+  &__submit-wrapper {
+    margin: 0 -1.25rem -1.25rem -1.25rem;
+  }
+
+  &__submit {
+    // margin: 0;
+    // margin-bottom: -1.25rem;
+    // // margin: 0 -1.25rem -1.25rem -1.25rem;
+  }
+}
+</style>
