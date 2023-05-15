1758557c36b5005983564cddcb60606e1ca5a488
Seungyoon66 <tmddbs1998@naver.com>
2023-05-15 20:24:30 +0900

    feat(components): NoteButton 추가

---
+++ components/Note/Button.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <button class="note-button">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.note-button {
+  display: block;
+  width: 100%;
+  border:none;
+  background: none;
+  background-color: var(--primary-color);
+  color: white;
+  padding: .9375rem;
+  font-weight: 500;
+  transition: background-color .2s;
+
+  &:hover {
+    background-color: var(--primary-color-dark);
+  }
+}
+</style>
