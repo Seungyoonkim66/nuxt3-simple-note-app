70b3c65ed77ac84f423ab6cd0edba6ddada982df
Seungyoon66 <tmddbs1998@naver.com>
2023-05-22 19:11:17 +0900

    feat(pages): note 추가 api 연동

--- pages/notes/new.vue
+++ pages/notes/new.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
-function submit(form: {title: string, content: string}) {
-  console.debug(form);
+import type { Note } from '@/types/note.type';
+import type { Response } from '@/types/response.type';
+
+const route = useRoute();
+const config = useRuntimeConfig(); // nuxt.config.js에 접근하기 위해 
+
+const id = computed(() => route.params.id);
+const apiUrl = config.public.apiUrl;
+
+async function submit(form: {title: string, content: string}) {
+  const { data } = await $fetch<Response<Note>>(`${apiUrl}/api/v1/notes`, {
+    method: 'POST',
+    body: {
+      title: form.title,
+      content: form.content
+    }
+  });
+
+  navigateTo(`/notes/${data.id}`)
 }
 </script>
 
