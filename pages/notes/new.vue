c08220cdcf61e3279de47f32e67125397216553d
Seungyoon66 <tmddbs1998@naver.com>
2023-05-22 19:20:39 +0900

    style: useFetch 관련 주석 추가

--- pages/notes/new.vue
+++ pages/notes/new.vue
@@ -9,6 +9,7 @@ const id = computed(() => route.params.id);
 const apiUrl = config.public.apiUrl;
 
 async function submit(form: {title: string, content: string}) {
+  /* 조회인 경우에는 useFetch를 쓰지만 데이터 변경이 있는 경우 fetch를 쓴다. */
   const { data } = await $fetch<Response<Note>>(`${apiUrl}/api/v1/notes`, {
     method: 'POST',
     body: {
