f3201eb326b0edc6fb4388db227b0b871b19d80b
Seungyoon66 <tmddbs1998@naver.com>
2023-07-04 19:10:43 +0900

    feat(schema): IdSchema에 number 타입으로 변경하는 로직 추가

--- server/schemas/params.schema.ts
+++ server/schemas/params.schema.ts
@@ -1,4 +1,18 @@
 import { z } from 'zod';
 
-export const IdSchema = z.string();
+export const IdSchema = z.string().transform((val, ctx) => {
+  // REF: number type이 아닌 아이디를 받았을 때 400 에러로 보낼 수 있도록 함 
+  const parsed = Number(val);
+
+  if (isNaN(parsed)) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'Invalid ID'
+    });
+
+    return z.NEVER;
+  }
+
+  return parsed;
+});
 
