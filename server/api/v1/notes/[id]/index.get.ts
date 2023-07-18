972cb91379081b9e93a128f51389164b810e5663
Seungyoon66 <tmddbs1998@naver.com>
2023-07-18 19:35:32 +0900

    refactor(db): note 조회 쿼리 최적화

--- server/api/v1/notes/[id]/index.get.ts
+++ server/api/v1/notes/[id]/index.get.ts
@@ -1,5 +1,5 @@
 import { IdSchema } from '~/server/schemas/params.schema';
-import { eq } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 import { db } from '~/server/db';
 import { notes as notesTable } from '~/server/db/schema'; 
 // import { supabase } from '~/server/supabase';
@@ -14,19 +14,42 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
-  const notes = await db.select()
-  .from(notesTable)
-  .where(eq(notesTable.id, parsedId.data));
-  // const { data: notes, error } = await supabase.from('notes').select().eq('id', parsedId.data);
+  // const notes = await db.select()
+  // .from(notesTable)
+  // .where(eq(notesTable.id, parsedId.data));
+  // // const { data: notes, error } = await supabase.from('notes').select().eq('id', parsedId.data);
 
-  // if (error) {
-  //   // log 코드 
-  //   console.error(error.message);
-  //   return sendError(event, createError({
-  //     statusCode: 500,
-  //     statusMessage: 'Internal server error'
-  //   }))
-  // }
+  // // if (error) {
+  // //   // log 코드 
+  // //   console.error(error.message);
+  // //   return sendError(event, createError({
+  // //     statusCode: 500,
+  // //     statusMessage: 'Internal server error'
+  // //   }))
+  // // }
+
+  // const currentViews = notes[0].views;
+
+  // const updatedNotes = await db.update(notesTable)
+  //   .set({ views: currentViews + 1 })
+  //   .where(eq(notesTable.id, parsedId.data))
+  //   .returning();
+
+  // // const { data: updatedNotes, error: updateError } = await supabase.from('notes').update({
+  // //   views: currentViews + 1
+  // // }).eq('id', notes[0].id).select();
+
+  // // if (updateError) {
+  // //   return sendError(event, createError({
+  // //     statusCode: 500,
+  // //     statusMessage: 'Internal server error'
+  // //   }));
+  // // }
+
+  const notes = await db.update(notesTable)
+    .set({ views: sql`views + 1` })
+    .where(eq(notesTable.id, parsedId.data))
+    .returning();
 
   if (notes.length === 0) {
     return sendError(event, createError({
@@ -35,25 +58,8 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
-  const currentViews = notes[0].views;
-
-  const updatedNotes = await db.update(notesTable)
-    .set({ views: currentViews + 1 })
-    .where(eq(notesTable.id, parsedId.data))
-    .returning();
-
-  // const { data: updatedNotes, error: updateError } = await supabase.from('notes').update({
-  //   views: currentViews + 1
-  // }).eq('id', notes[0].id).select();
-
-  // if (updateError) {
-  //   return sendError(event, createError({
-  //     statusCode: 500,
-  //     statusMessage: 'Internal server error'
-  //   }));
-  // }
 
   return {
-    data: updatedNotes[0],
+    data: notes[0],
   }
 })
