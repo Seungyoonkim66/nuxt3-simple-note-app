5d8e122497be8c3340fe51695a5a90d86e1ea36e
Seungyoon66 <tmddbs1998@naver.com>
2023-07-18 19:18:01 +0900

    refactor(db): note 조회 api supabase -> drizzle orm으로 변경

--- server/api/v1/notes/[id]/index.get.ts
+++ server/api/v1/notes/[id]/index.get.ts
@@ -1,5 +1,8 @@
 import { IdSchema } from '~/server/schemas/params.schema';
-import { supabase } from '~/server/supabase';
+import { eq } from 'drizzle-orm';
+import { db } from '~/server/db';
+import { notes as notesTable } from '~/server/db/schema'; 
+// import { supabase } from '~/server/supabase';
 
 export default defineEventHandler(async (event) => {
   const parsedId = IdSchema.safeParse(event.context.params?.id);
@@ -11,16 +14,19 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
-  const { data: notes, error } = await supabase.from('notes').select().eq('id', parsedId.data);
+  const notes = await db.select()
+  .from(notesTable)
+  .where(eq(notesTable.id, parsedId.data));
+  // const { data: notes, error } = await supabase.from('notes').select().eq('id', parsedId.data);
 
-  if (error) {
-    // log 코드 
-    console.error(error.message);
-    return sendError(event, createError({
-      statusCode: 500,
-      statusMessage: 'Internal server error'
-    }))
-  }
+  // if (error) {
+  //   // log 코드 
+  //   console.error(error.message);
+  //   return sendError(event, createError({
+  //     statusCode: 500,
+  //     statusMessage: 'Internal server error'
+  //   }))
+  // }
 
   if (notes.length === 0) {
     return sendError(event, createError({
@@ -31,16 +37,21 @@ export default defineEventHandler(async (event) => {
 
   const currentViews = notes[0].views;
 
-  const { data: updatedNotes, error: updateError } = await supabase.from('notes').update({
-    views: currentViews + 1
-  }).eq('id', notes[0].id).select();
+  const updatedNotes = await db.update(notesTable)
+    .set({ views: currentViews + 1 })
+    .where(eq(notesTable.id, parsedId.data))
+    .returning();
 
-  if (updateError) {
-    return sendError(event, createError({
-      statusCode: 500,
-      statusMessage: 'Internal server error'
-    }));
-  }
+  // const { data: updatedNotes, error: updateError } = await supabase.from('notes').update({
+  //   views: currentViews + 1
+  // }).eq('id', notes[0].id).select();
+
+  // if (updateError) {
+  //   return sendError(event, createError({
+  //     statusCode: 500,
+  //     statusMessage: 'Internal server error'
+  //   }));
+  // }
 
   return {
     data: updatedNotes[0],
