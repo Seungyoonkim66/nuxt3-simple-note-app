4f0faca214070bc50957ef77667212595194642a
Seungyoon66 <tmddbs1998@naver.com>
2023-07-18 19:21:37 +0900

    chore(db): note 수정 api supabase -> drizzle orm으로 변경

--- server/api/v1/notes/[id]/index.put.ts
+++ server/api/v1/notes/[id]/index.put.ts
@@ -1,8 +1,10 @@
 import { IdSchema } from '~/server/schemas/params.schema';
 import { NoteSchema } from '~/server/schemas/note.schema';
-import { supabase } from '~/server/supabase';
+// import { supabase } from '~/server/supabase';
 import { createTeaser, extractTextFromHtml } from "~/server/utils/teaser.util";
-
+import { db } from '~/server/db';
+import { notes, notes as notesTable } from '~/server/db/schema';
+import { eq } from 'drizzle-orm';
 
 export default defineEventHandler(async (event) => {
   const parsedId = IdSchema.safeParse(event.context.params?.id);
@@ -23,20 +25,30 @@ export default defineEventHandler(async (event) => {
       statusMessage: note.error.message
     }));
   }
+  
+  const updatedNotes = await db.update(notesTable)
+    .set({
+      title: note.data.title,
+      content: note.data.content,
+      teaser: createTeaser(extractTextFromHtml(note.data.content), 100),
+      updated_at: new Date(),
+    })
+    .where(eq(notesTable.id, parsedId.data))
+    .returning({ id: notesTable.id });
+    
+  // const { data:updatedNotes, error } = await supabase.from('notes').update({
+    // title: note.data.title,
+    // content: note.data.content,
+    // teaser: createTeaser(extractTextFromHtml(note.data.content), 100),
+    // updated_at: new Date().toISOString(),
+  // }).eq('id', parsedId.data).select('id');
 
-  const { data:updatedNotes, error } = await supabase.from('notes').update({
-    title: note.data.title,
-    content: note.data.content,
-    teaser: createTeaser(extractTextFromHtml(note.data.content), 100),
-    updated_at: new Date().toISOString(),
-  }).eq('id', parsedId.data).select('id');
-
-  if (error) {
-    return sendError(event, createError({
-      statusCode: 500,
-      statusMessage: 'Internal server error',
-    }));
-  }
+  // if (error) {
+  //   return sendError(event, createError({
+  //     statusCode: 500,
+  //     statusMessage: 'Internal server error',
+  //   }));
+  // }
 
   if (updatedNotes.length === 0) {
     return sendError(event, createError({
