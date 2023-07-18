f3a98d129a937ecf80ee94c1ec9417b5db52f4f2
Seungyoon66 <tmddbs1998@naver.com>
2023-07-18 19:24:46 +0900

    chore(db): note 삭제 api supabase -> drizzle orm으로 변경

--- server/api/v1/notes/[id]/index.delete.ts
+++ server/api/v1/notes/[id]/index.delete.ts
@@ -1,5 +1,8 @@
 import { IdSchema } from "~/server/schemas/params.schema"
-import { supabase } from "~/server/supabase";
+// import { supabase } from "~/server/supabase";
+import { db } from '~/server/db';
+import { notes as notesTable } from '~/server/db/schema';
+import { eq } from "drizzle-orm";
 
 export default defineEventHandler(async (event) => {
   const parsedId = IdSchema.safeParse(event.context.params?.id);
@@ -11,18 +14,20 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
-  const { error } = await supabase.from('notes').delete().eq('id', parsedId.data);
 
-  if (error) {
-    return sendError(event, createError({
-      statusCode: 500,
-      statusMessage: 'Internal serever error'
-    }));
-  }
+  const deletedNote = await db.delete(notesTable)
+    .where(eq(notesTable.id, parsedId.data));
+
+  // const { error } = await supabase.from('notes').delete().eq('id', parsedId.data);
+
+  // if (error) {
+  //   return sendError(event, createError({
+  //     statusCode: 500,
+  //     statusMessage: 'Internal serever error'
+  //   }));
+  // }
 
   return {
-    data: {
-      id: parsedId.data
-    }
+    data: "success"
   }
 })
