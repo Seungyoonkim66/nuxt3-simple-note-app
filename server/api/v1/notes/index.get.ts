97ffd377983c72158a00178076566f5e04d2c0bb
Seungyoon66 <tmddbs1998@naver.com>
2023-07-18 19:05:26 +0900

    refactor(db): notes 조회 supabase -> drizzle orm으로 변경

--- server/api/v1/notes/index.get.ts
+++ server/api/v1/notes/index.get.ts
@@ -1,11 +1,14 @@
-import { supabase } from "~/server/supabase";
-
+// import { supabase } from "~/server/supabase";
+import { db } from '~/server/db';
+import { notes as notesTable } from '~/server/db/schema';
 
 export default defineEventHandler(async (event) => {
 
-  const { data: notes, error } = await supabase
-    .from('notes')
-    .select();
+  const notes = await db.select().from(notesTable);
+
+  // const { data: notes, error } = await supabase
+  //   .from('notes')
+  //   .select();
 
   return {
     data: notes,
