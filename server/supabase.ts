7fb072f7885010ab5b69d51edea799c4d9ec9c9d
Seungyoon66 <tmddbs1998@naver.com>
2023-07-18 19:28:18 +0900

    refactor(db): supabase 레거시 코드 제거

--- server/supabase.ts
+++ server/supabase.ts
@@ -1,12 +1,12 @@
 
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
-const { supabaseUrl, supabaseApiKey } = useRuntimeConfig();
+// const { supabaseUrl, supabaseApiKey } = useRuntimeConfig();
 
-// Create a single supabase client for interacting with your database
-// Project settings > api settings, env에 저장하고 nuxt.config.ts에서 runTimeConfig내에 선언해서 불러와서 쓰기
-export const supabase = createClient(supabaseUrl, supabaseApiKey, {
-  auth: {
-    persistSession: false,
-  }
-});
+// // Create a single supabase client for interacting with your database
+// // Project settings > api settings, env에 저장하고 nuxt.config.ts에서 runTimeConfig내에 선언해서 불러와서 쓰기
+// export const supabase = createClient(supabaseUrl, supabaseApiKey, {
+//   auth: {
+//     persistSession: false,
+//   }
+// });
